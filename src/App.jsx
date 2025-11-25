3915e0dca959bbfcf14a416e8606b3c69cacfb28
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-11-25 17:40:25 +0600

    feat: enhance winning number logic by integrating result121 data and updating winning numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import "./App.css";
 import data1 from "./data/draw1.json";
 import data2 from "./data/draw2.json";
 import data3 from "./data/draw3.json";
+import result121 from "./data/drawResults/result121.json";
 import winner from "./data/winingNumber.json";
 
 function App() {
-  const winningNumber = [...winner].map((item) => item.id);
+  const winningNumber = [...winner, ...result121].map((item) => item.id);
   const allData = [...data1, ...data2, ...data3].map((item) => item.bond);
 
   function check() {
     for (let number of winningNumber) {
       if (allData.includes(number)) {
         alert(`Congrats ${number} won the lottery`);
-        console.log("congrats you won the lottery");
-        // console.log(number, "for");
+        console.log(`Congrats ${number} won the lottery`);
+      } else {
+        console.log("ohh you lost the lottery");
       }
-      //   console.log("not for", allData.includes(number));
     }
-    // alert("ohh you lost the lottery");
-    console.log("ohh you lost the lottery");
   }
 
   return (
